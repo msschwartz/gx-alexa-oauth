bfea5552ae20f771427757c40bdf0fe9192e3b02
Schwartz <msschwartz@outlook.com>
2017-06-27 13:10:43 -0400

    redirect

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 error_log(print_r($_SERVER, true));
 
-echo 'hello';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	$state = $_REQUEST['state'];
+	$redirect = $_REQUEST['redirect_uri'];
+	$code = 'xyz';
+	
+	header( "Location: {$redirect}?code={$code}&state={$state}" ) ;
+	exit;
+}
+
+?><html>
+<body>
+	<form method="post" action="/">
+		<input type="hidden" name="state" value="<?php echo $_REQUEST['state'] ?>" />
+		<input type="hidden" name="redirect_uri" value="<?php echo $_REQUEST['redirect_uri'] ?>" />
+		<input type="submit" />
+	</form>
+</body>
+</html>
